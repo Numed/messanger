ccadda48ac1bf443f751683c97a85658a2da7c9b
Numed <ruschinets09@gmail.com>
2023-03-07 00:52:07 +0200

    refactore project

--- src/services/index.jsx
+++ src/services/index.jsx
@@ -1,39 +1,39 @@
 import {
-    Date,
-    MyMessage,
-    MyMessageText,
-    MyContainer,
-    MyMessageContainer,
-    InterlocutorContainer,
-    InterlocutorMessage,
-    MessageContainer,
-    MessageText,
-  } from "../components/ChatSide/style";
-  import { Avatar } from "../components/SideMenu/style";
+  Date,
+  MyMessage,
+  MyMessageText,
+  MyContainer,
+  MyMessageContainer,
+  InterlocutorContainer,
+  InterlocutorMessage,
+  MessageContainer,
+  MessageText,
+} from "../components/ChatSide/style";
+import { Avatar } from "../components/SideMenu/style";
 
 export const CreateMyMessage = ({ message, dateNow }) => {
-    return (
-      <MyContainer className="my-container">
-        <MyMessageContainer>
-          <MyMessage className="my-message">
-            <MyMessageText>{message}</MyMessageText>
-          </MyMessage>
-          <Date className="message-date">{dateNow}</Date>
-        </MyMessageContainer>
-      </MyContainer>
-    );
-  };
-  
-  export const CreateInterlocutorMessage = ({ avatar, name, value, dateNow }) => {
-    return (
-      <InterlocutorContainer className="interlocutor-container">
-        <Avatar src={avatar} alt={name} />
-        <MessageContainer>
-          <InterlocutorMessage className="message-text">
-            <MessageText>{value}</MessageText>
-          </InterlocutorMessage>
-          <Date className="message-date">{dateNow}</Date>
-        </MessageContainer>
-      </InterlocutorContainer>
-    );
-  };
+  return (
+    <MyContainer className="my-container">
+      <MyMessageContainer>
+        <MyMessage className="my-message">
+          <MyMessageText>{message}</MyMessageText>
+        </MyMessage>
+        <Date className="message-date">{dateNow}</Date>
+      </MyMessageContainer>
+    </MyContainer>
+  );
+};
+
+export const CreateInterlocutorMessage = ({ avatar, name, value, dateNow }) => {
+  return (
+    <InterlocutorContainer className="interlocutor-container">
+      <Avatar src={avatar} alt={name} />
+      <MessageContainer>
+        <InterlocutorMessage className="message-text">
+          <MessageText>{value}</MessageText>
+        </InterlocutorMessage>
+        <Date className="message-date">{dateNow}</Date>
+      </MessageContainer>
+    </InterlocutorContainer>
+  );
+};
